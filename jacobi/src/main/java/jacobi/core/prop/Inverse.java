92436bc21f1e404f1281304387aaa630cb2102a2
Edwin <Edwin@Temp-DEV>
2016-10-23 18:36:04 +0800

    Fix computations on small matrices, and decouple with implementation

--- jacobi/src/main/java/jacobi/core/prop/Inverse.java
+++ jacobi/src/main/java/jacobi/core/prop/Inverse.java
@@ -25,8 +25,7 @@ package jacobi.core.prop;
 
 import jacobi.api.Matrices;
 import jacobi.api.Matrix;
-import jacobi.core.decomp.gauss.FullMatrixOperator;
-import jacobi.core.decomp.gauss.GenericGaussianElim;
+import jacobi.core.decomp.gauss.GaussianDecomp;
 import jacobi.core.impl.DefaultMatrix;
 import jacobi.core.solver.Substitution;
 import jacobi.core.util.Throw;
@@ -41,20 +40,35 @@ import java.util.Optional;
  */
 public class Inverse {
 
+    /**
+     * Constructor.
+     */
     public Inverse() {
-        this.gaussElim = new GenericGaussianElim();
+        this.gaussDecomp = new GaussianDecomp();
     }
     
+    /**
+     * Compute the inverse of input matrix A.
+     * @param a  Input matrix A
+     * @return  A^-1, or empty if A is not squared or singular.
+     */
     public Optional<Matrix> computeMaybe(Matrix a) {
         return Optional.ofNullable(this.compute(a));
     }
     
-    public Matrix compute(Matrix a) {
+    /**
+     * Compute the inverse of input matrix A with null return as singular.
+     * @param a  Input matrix A
+     * @return  A^-1, or null if A is not squared or singular
+     */
+    protected Matrix compute(Matrix a) {
         Throw.when()
             .isNull(() -> a, () -> "No matrix to invert.");
-        if(a.getRowCount() != a.getColCount()
-        || a.getRowCount() == 0){
+        if(a.getRowCount() != a.getColCount()){
             return null;
+        }
+        if(a.getRowCount() == 0){
+            return a;
         }
         switch(a.getRowCount()){
             case 0 : throw new IllegalStateException();
@@ -65,59 +79,70 @@ public class Inverse {
                 break;
         }
         Matrix y = Matrices.identity(a.getRowCount());
-        this.gaussElim.compute(a, (op) -> new FullMatrixOperator(op, y));
-        
+        this.gaussDecomp.compute(a, y);
         return new Substitution(Substitution.Mode.BACKWARD, a).compute(y);
     }
 
-    private Matrix inverse1x1(Matrix a) {
-        return Math.abs(a.get(0, 0)) < 1e-12 ? null : Matrices.scalar(1.0 / a.get(0, 0));
+    /**
+     * Compute the inverse of a 1x1 matrix A.
+     * @param a  Input matrix A
+     * @return  A^-1, or null if A is not squared or singular
+     */
+    protected Matrix inverse1x1(Matrix a) {
+        return Math.abs(a.get(0, 0)) < EPSILON ? null : Matrices.scalar(1.0 / a.get(0, 0));
     }
     
-    private Matrix inverse2x2(Matrix a) {
+    /**
+     * Compute the inverse of a 2x2 matrix A.
+     * @param a  Input matrix A
+     * @return  A^-1, or null if A is not squared or singular
+     */
+    protected Matrix inverse2x2(Matrix a) {
         double[] r0 = a.getRow(0);
-        double[] r1 = a.getRow(0);
+        double[] r1 = a.getRow(1);
         double det = r0[0] * r1[1] - r0[1] * r1[0];
-        if(Math.abs(det) < 1e-12){
+        if(Math.abs(det) < EPSILON){
             return null;
         }
         return new DefaultMatrix(new double[][]{
-            {-r1[1] / det,  r0[0] / det },
-            { r1[0] / det, -r0[1] / det }
+            { r1[1] / det, -r0[1] / det },
+            {-r1[0] / det,  r0[0] / det }
         });
     }
     
-    private Matrix inverse3x3(Matrix a) {
+    /**
+     * Compute the inverse of a 3x3 matrix A.
+     * @param a  Input matrix A
+     * @return A^-1, or null if A is not squared or singular
+     */
+    protected Matrix inverse3x3(Matrix a) {
         double det = new Determinant().compute3x3(a);
-        if(Math.abs(det) < 1e-12){
+        if(Math.abs(det) < EPSILON){
             return null;
         }
         double[] r0 = a.getRow(0);
         double[] r1 = a.getRow(1);
         double[] r2 = a.getRow(2);
         return new DefaultMatrix(new double[][]{
-            
             { 
-                (r1[1]*r2[2] - r2[1]*r1[2]) / det ,
-               //-(r0[1]*r2[2] - r2[1]*r1[2]) / det ,
-                (r2[1]*r1[2] - r0[1]*r2[2]) / det ,
-                (r0[1]*r2[2] - r1[1]*r1[2]) / det ,
+                (r1[1]*r2[2] - r1[2]*r2[1])/det,
+                (r0[2]*r2[1] - r0[1]*r2[2])/det,
+                (r0[1]*r1[2] - r0[2]*r1[1])/det
             },
             {
-               //-(r1[0]*r2[2] - r2[0]*r1[2]) / det ,
-                (r2[0]*r1[2] - r1[0]*r2[2]) / det ,
-                (r0[0]*r2[2] - r2[0]*r1[2]) / det ,
-               //-(r0[0]*r2[2] - r1[0]*r1[2]) / det 
-                (r1[0]*r1[2] - r0[0]*r2[2]) / det 
+                (r2[0]*r1[2] - r1[0]*r2[2])/det,
+                (r0[0]*r2[2] - r0[2]*r2[0])/det,
+                (r0[2]*r1[0] - r0[0]*r1[2])/det
             },
             {
-                (r1[0]*r2[1] - r2[0]*r1[1]) / det ,
-               //-(r0[0]*r2[1] - r2[0]*r1[1]) / det ,
-                (r2[0]*r1[1] - r0[0]*r2[1]) / det ,
-                (r0[0]*r2[1] - r1[0]*r1[1]) / det 
+                (r1[0]*r2[1] - r1[1]*r2[0])/det,
+                (r0[1]*r2[0] - r0[0]*r2[1])/det,
+                (r0[0]*r1[1] - r0[1]*r1[0])/det
             }
         });
     }
     
-    private GenericGaussianElim gaussElim;
+    private GaussianDecomp gaussDecomp;
+    
+    private static final double EPSILON = 1e-12;
 }
