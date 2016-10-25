8d193b116c5c6465e10cb7d31feb9cb782755d80
Edwin <Edwin@Edwin-PC>
2016-10-25 13:40:24 +0800

    Add switch to single shift when first column / last row almost deflated

---
+++ jacobi/src/test/java/jacobi/core/decomp/qr/FullQRTest.java
@@ -0,0 +1,74 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 Y.K. Chan.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package jacobi.core.decomp.qr;
+
+import jacobi.api.Matrix;
+import jacobi.core.decomp.qr.step.QRStep;
+import jacobi.test.annotations.JacobiEquals;
+import jacobi.test.annotations.JacobiImport;
+import jacobi.test.annotations.JacobiInject;
+import jacobi.test.annotations.JacobiResult;
+import jacobi.test.util.Jacobi;
+import jacobi.test.util.JacobiJUnit4ClassRunner;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ *
+ * @author Y.K. Chan
+ */
+@JacobiImport("/jacobi/test/data/FullQRTest.xlsx")
+@RunWith(JacobiJUnit4ClassRunner.class)
+public class FullQRTest {
+    
+    @JacobiInject(-1)
+    public Map<Integer, Matrix> steps;
+    
+    @JacobiResult(100)
+    public Matrix result;
+    
+    @Test
+    @JacobiImport("Full 4x4")
+    @JacobiEquals(expected = 100, actual = 100)
+    public void test4x4() {
+        Matrix input = this.steps.get(0);
+        this.mock().compute(input, null, true);
+        this.result = input;
+    }
+    
+    protected FullQR mock() {
+        AtomicInteger count = new AtomicInteger(0);
+        QRStep step = new FullQR().getStep();
+        return new FullQR((m, p, begin, end, full) -> {
+            int k = count.incrementAndGet(); 
+            if(steps.containsKey(k)){                
+                Jacobi.assertEquals(steps.get(k), m);
+            }
+            step.compute(m, p, begin, end, full);
+            
+        });
+    }
+}
