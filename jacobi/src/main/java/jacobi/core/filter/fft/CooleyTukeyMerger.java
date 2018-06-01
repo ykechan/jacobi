3e1df379484db14189476930b62f8428c303c329
ykechan <ykechan@gmail.com>
2018-06-01 17:40:41 +0800

    Cooley Tukey FFT

---
+++ jacobi/src/main/java/jacobi/core/filter/fft/CooleyTukeyMerger.java
@@ -0,0 +1,42 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2018 Y.K. Chan.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package jacobi.core.filter.fft;
+
+/**
+ * Interface for merging split parts of vector by Cooley-Tukey algorithm.
+ *
+ * @author Y.K. Chan
+ */
+public interface CooleyTukeyMerger {
+
+    /**
+     * Merge split parts of vector.
+     * @param vector  Complex vector to be merged
+     * @param offset  Offset index of the range
+     * @param length  Length of the range
+     */
+    public void merge(ComplexVector vector, int offset, int length);
+
+}
