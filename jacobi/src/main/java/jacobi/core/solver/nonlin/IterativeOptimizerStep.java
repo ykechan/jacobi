70ed9bf37493e572d11226c3fd7c06c8c9e32b81
eykchan1 <eykchan1@sfc.hk>
2019-05-02 18:08:37 +0800

    Update class name

--- jacobi/src/main/java/jacobi/core/solver/nonlin/IterativeOptimizerStep.java
+++ jacobi/src/main/java/jacobi/core/solver/nonlin/IterativeOptimizerStep.java
@@ -1,7 +1,22 @@
 package jacobi.core.solver.nonlin;
 
-public interface NonLinearOptimizingStep {
+/**
+ * Common interface for an iteration in a numeric iterative optimizer.
+ * 
+ * <p></p>
+ * 
+ * @author Y.K. Chan
+ *
+ */
+public interface IterativeOptimizerStep {
     
-    public double[] delta(VectorFunction func, double[] init);
+    /**
+     * Given the current position, find the delta vector to move to a more optimized
+     * position.
+     * @param func  Vector function to be optimized
+     * @param curr  Current position
+     * @return  Delta vector to move to new position
+     */
+    public double[] delta(VectorFunction func, double[] curr);
 
 }
