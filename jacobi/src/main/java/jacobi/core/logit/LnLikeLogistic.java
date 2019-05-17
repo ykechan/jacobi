9ab3963b91442b74b241968e134fca4ffc11d53d
eykchan1 <eykchan1@sfc.hk>
2019-05-17 16:51:44 +0800

    Re-factor Sum Linear Arg Function

---
+++ jacobi/src/main/java/jacobi/core/logit/LnLikeLogistic.java
@@ -0,0 +1,78 @@
+package jacobi.core.logit;
+
+import jacobi.api.Matrix;
+import jacobi.core.solver.nonlin.SumLinearArgFunc;
+
+/**
+ * Negated Log of Likelihood function of the logistic probabilistic model in a weighted Bernoulli trial. 
+ * 
+ * <p>
+ * Consider the logistic function p(x) = 1/(1 + e<sup>-x</sup>)<br>
+ * 
+ * p'(x) = -e<sup>-x</sup> -1/(1 + e<sup>-x</sup>)<sup>2</sup><br>
+ *       = e<sup>-x</sup>/(1 + e<sup>-x</sup>)<sup>2</sup><br>
+ *       = p(x)q(x), where q(x) = 1 - p(x)<br>
+ * </p>
+ * 
+ * <p>
+ * Given observations y<sub>k</sub> &isin; {0, 1}, the likelihood of the model is<br>
+ * 
+ * L(x) = &prod;p<sup>y<sub>k</sub></sup>(x)q<sup>1-y<sub>k</sub></sup>(x)<br>
+ * 
+ * Let f(x) = lnL(x) = &sum;y<sub>k</sub>ln(p) + (1 - y<sub>k</sub>)ln(q)<br>
+ * 
+ * f'(x) = &sum;y<sub>k</sub>p'/p + (1 - y<sub>k</sub>)q'/q<br>
+ *       = &sum;y<sub>k</sub>q - (1 - y<sub>k</sub>)p<br>
+ *       = &sum;y<sub>k</sub>(1 - p) - (1 - y<sub>k</sub>)p<br>
+ *       = &sum;y<sub>k</sub> - p<br>
+ *       
+ * f''(x) = &sum;-p(x)q(x)<br>
+ * </p>
+ * 
+ * <p>
+ * The variable x is a linear combination of some variables X and some observations A, i.e.
+ * x = &lt;A, X&gt; where A is matrix and X is a column vector. 
+ * </p>
+ * 
+ * <p>This class represents g(x) = -f(x) s.t. a maximum likelihood estimate can be
+ * obtained if g(x) is minimized.</p>
+ * 
+ * @author Y.K. Chan
+ *
+ */
+public class LnLikeLogistic extends SumLinearArgFunc<double[]> {
+
+    public LnLikeLogistic(Matrix consts, double[] weights) {
+        super(consts);
+    }
+
+    @Override
+    protected double valueAt(double[] inter, int index, double x) {
+        return this.weights[index] < 0.0 
+            ?  this.weights[index] * Math.log(1.0 - inter[index])
+            : -this.weights[index] * Math.log(inter[index]);
+    }
+
+    @Override
+    protected double slopeAt(double[] inter, int index, double x) {
+        return this.weights[index] < 0.0 
+                ? -this.weights[index] * inter[index]
+                :  this.weights[index] * (inter[index] - 1.0);
+    }
+
+    @Override
+    protected double convexityAt(double[] inter, int index, double x) {
+        return Math.abs(this.weights[index]) * inter[index] * (1.0 - inter[index]);
+    }
+
+    @Override
+    protected double[] prepare(double[] pos, double[] args) {
+        double[] probs = new double[args.length];
+        for(int i = 0; i < probs.length; i++) {
+            probs[i] = 1.0 / (1.0 + Math.exp(-args[i] * args[i]));
+        }
+        return probs;
+    }
+
+    private double[] weights;
+}
--- jacobi/src/main/java/jacobi/core/solver/nonlin/SumLinearArgFunc.java
+++ jacobi/src/main/java/jacobi/core/solver/nonlin/SumLinearArgFunc.java
@@ -1,17 +1,17 @@
 package jacobi.core.solver.nonlin;
 
 import java.util.Arrays;
-import java.util.function.Function;
-import java.util.function.IntFunction;
+import java.util.stream.IntStream;
 
 import jacobi.api.Matrices;
 import jacobi.api.Matrix;
 import jacobi.core.impl.ColumnVector;
 import jacobi.core.op.MulT;
 import jacobi.core.util.MapReducer;
+import jacobi.core.util.Throw;
 
 /**
- * A non-linear vector function f(X) 
+ * A non-linear vector function f(X) with form specified as follows.
  * 
  * <p>
  * This class refers to a function that takes the form<br>
@@ -30,135 +30,162 @@ import jacobi.core.util.MapReducer;
  *         = [ Sum{A<sub>k</sub><sup>(i)</sup> A<sub>k</sub><sup>(j)</sup> g''<sub>k</sub>} ]
  * </p>
  * 
+ * 
  * @author Y.K. Chan
  *
  */
-public class SumLinearArgFunc implements VectorFunction {
+public abstract class SumLinearArgFunc<T> implements VectorFunction {
     
     /**
-     * Constructor
-     * @param consts  Argument coefficients 
-     * @param funcFact  Function factory
+     * Constructor.
+     * @param consts  Linear coefficients for variables
      */
-    public SumLinearArgFunc(Matrix consts, Function<double[], FunctionSeries> funcFact) {
-        this(consts, funcFact, new MulT(), MapReducer.DEFAULT_NUM_FLOP);
+    public SumLinearArgFunc(Matrix consts) {
+        this(consts, new MulT(), MapReducer.DEFAULT_NUM_FLOP);
     }
 
     /**
-     * Constructor
-     * @param consts  Argument coefficients
-     * @param funcFact  Function factory
-     * @param mulT  Transpose multiplication implementation
-     * @param flopThres  Threshold of number of FLOP to use parallelism
+     * Constructor.
+     * @param consts  Linear coefficients for variables
+     * @param mulT  Transpose multiplication implementation 
+     * @param flopThres  Threshold of FLOP count to use parallelism
      */
-    protected SumLinearArgFunc(Matrix consts, 
-            Function<double[], FunctionSeries> funcFact, 
-            MulT mulT,
-            int flopThres) {
+    protected SumLinearArgFunc(Matrix consts, MulT mulT, int flopThres) {
         this.consts = consts;
-        this.funcFact = funcFact;
         this.mulT = mulT;
         this.flopThres = flopThres;
     }
 
     @Override
     public double at(double[] pos) {
-        Cache cache = this.getEntry(pos);
-        double value = 0.0;
-        for(int i = 0; i < cache.args.length; i++) {
-            value += cache.series.valueAt(i, cache.args[i]);
+        Params<T> params = this.prepare(pos);
+        double fx = 0.0;
+        for(int i = 0; i < params.args.length; i++) {
+            fx += this.valueAt(params.inter, i, params.args[i]);
         }
-        return value;
+        return fx;
     }
 
     @Override
-    public ColumnVector grad(double[] pos) {        
-        Cache entry = this.getEntry(pos);
-        if(pos.length * this.consts.getRowCount() > this.flopThres) {
-            double[] ans = MapReducer.of(0, this.consts.getRowCount())
-                .flop(pos.length)
-                .map((begin, end) -> this.grad(entry.series, entry.args, begin, end))
-                .reduce((u, v) -> {
-                    for(int i = 0; i < u.length; i++) {
-                        u[i] += v[i];
-                    }
-                    return u;
-                })
-                .get();
-            return new ColumnVector(ans);
-        }        
-        double[] ans = this.grad(entry.series, entry.args, 0, this.consts.getRowCount());
-        return new ColumnVector(ans);
+    public ColumnVector grad(double[] pos) {
+        Params<T> params = this.prepare(pos);
+        
+        double[] vector = pos.length * this.consts.getRowCount() > this.flopThres
+            ? MapReducer.of(0, this.consts.getRowCount())
+                  .flop(pos.length)
+                  .map((begin, end) -> this.grad(params, pos, begin, end))
+                  .reduce(this::merge)
+                  .get()
+            : this.grad(params, pos, 0, this.consts.getRowCount());
+                  
+        return new ColumnVector(vector);
     }
 
     @Override
     public Matrix hess(double[] pos) {
-        Cache entry = this.getEntry(pos);
+        Params<T> params = this.prepare(pos);
+        
         int totalFlop = (pos.length * pos.length * this.consts.getRowCount()) / 2;
-        if(totalFlop > this.flopThres) {
-            Matrix mat = MapReducer.of(0, entry.args.length)
-                .flop((pos.length * pos.length) / 2)
-                .map((begin, end) -> this.hess(entry.series, entry.args, begin, end))
-                .reduce((a, b) -> {
-                    return a;
-                })
-                .get();
-            return this.symmetrize(mat);
-        }
-        return this.symmetrize(this.hess(entry.series, entry.args, 0, entry.args.length));
+        
+        Matrix mat = totalFlop > this.flopThres
+             ? MapReducer.of(0, this.consts.getRowCount())
+                  .flop((pos.length * pos.length) / 2)
+                  .map((begin, end) -> this.hess(params, pos, begin, end))
+                  .reduce(this::merge)
+                  .get()
+             : this.hess(params, pos, 0, this.consts.getRowCount());
+        
+        return this.symmetrize(mat);
     }
     
     /**
-     * Compute sum of the gradient for a range of functions
-     * @param series  Function series
-     * @param args  Function arguments
+     * Compute the sum of gradient components of given range
+     * @param params  Argument for input position
+     * @param pos  Input position
      * @param begin  Begin index of range
      * @param end  End index of range
-     * @return  Sum of the gradients of the range of functions
+     * @return  Sum of gradient components
      */
-    protected double[] grad(FunctionSeries series, double[] args, int begin, int end) {
-        double[] vector = new double[this.consts.getColCount()];
+    protected double[] grad(Params<T> params, double[] pos, int begin, int end) {
+        double[] vector = new double[pos.length];
         for(int i = begin; i < end; i++) {
-            double df = series.slopeAt(i, args[i]);
-            double[] coeff = this.consts.getRow(i);
+            double dx = this.slopeAt(params.inter, i, params.args[i]);
+            double[] row = this.consts.getRow(i);
             for(int j = 0; j < vector.length; j++) {
-                vector[j] += df * coeff[j];
+                vector[j] += dx * row[j];
             }
         }
         return vector;
     }
-    
+
     /**
-     * Compute sum of the upper Hessian for a range of functions
-     * @param series  Function series
-     * @param args  Function arguments
+     * Compute the sum of Hessian components of given range. Since Hessian is symmetric, only
+     * the upper part is computed.
+     * @param params  Argument for input position
+     * @param pos  Input position
      * @param begin  Begin index of range
      * @param end  End index of range
-     * @return  Sum of the upper Hessian matrix of the range of functions
+     * @return  Sum of Hessian components
      */
-    protected Matrix hess(FunctionSeries series, double[] args, int begin, int end) {
-        Matrix mat = Matrices.zeros(this.consts.getColCount());
+    protected Matrix hess(Params<T> params, double[] pos, int begin, int end) {
+        Matrix mat = Matrices.zeros(pos.length);
+        
         for(int k = begin; k < end; k++) {
             double[] coeff = this.consts.getRow(k);
-            double conv = series.convexityAt(k, args[k]);
+            double d2x = this.convexityAt(params.inter, k, params.args[k]);
+            
             for(int i = 0; i < mat.getRowCount(); i++) {
                 double[] row = mat.getRow(i);
                 for(int j = i; j < row.length; j++) {
-                    row[j] += conv * coeff[i] * coeff[j];
+                    
+                    row[j] += d2x * coeff[i] * coeff[j];
                 }
                 mat.setRow(i, row);
             }
         }
+        
         return mat;
     }
     
     /**
-     * Turn the matrix into a symmetric matrix by copying the upper part to the lower part.
+     * Merge two vectors to the first vector as sum
+     * @param u  First vector
+     * @param v  Second vector
+     * @return  First vector with sum as values
+     */
+    protected double[] merge(double[] u, double[] v) {
+        for(int i = 0; i < u.length; i++) {
+            u[i] += v[i];
+        }
+        return u;
+    }
+    
+    /**
+     * Merge two matrices to the first matrix as sum of upper part.
+     * @param left  First matrix
+     * @param right  Second matrix
+     * @return  First matrix with sum of upper part
+     */
+    protected Matrix merge(Matrix left, Matrix right) {
+        for(int i = 0; i < left.getRowCount(); i++) {            
+            double[] leftRow = left.getRow(i);
+            double[] rightRow = right.getRow(i);
+            
+            for(int j = i; j < leftRow.length; j++) {
+                leftRow[j] += rightRow[j];
+            }
+            left.setRow(i, leftRow);
+        }
+        return left;
+    }
+    
+    /**
+     * Make a symmetric matrix by copying the upper part to the lower part.
      * @param mat  Input matrix
-     * @return  Symmetrized matrix
+     * @return  Input matrix with lower part copied
      */
     protected Matrix symmetrize(Matrix mat) {
-        for(int i = 0; i < mat.getRowCount(); i++){
+        for(int i = 0; i < mat.getRowCount(); i++) {
             double[] row = mat.getRow(i);
             for(int j = 0; j < i; j++) {
                 row[j] = mat.get(j, i);
@@ -169,33 +196,49 @@ public class SumLinearArgFunc implements VectorFunction {
     }
     
     /**
-     * Get cache entry which includes function series and function arguments at a given position.
+     * Prepare function arguments. If the position is exactly equals to the previous 
+     * position, a cached is returned.
      * @param pos  Input position
-     * @return  Cache entry
+     * @return  Function arguments
      */
-    protected Cache getEntry(double[] pos) {
-        Cache entry = this.prevEntry;
-        if(entry == null || this.deepEquals(entry.pos, pos)) {
-            double[] args = this.linearArgs(pos);
-            FunctionSeries series = this.funcFact.apply(args);
-            Cache currEntry = new Cache(
-                Arrays.copyOf(pos, pos.length),
-                args,
-                series
-            );
-            this.prevEntry = currEntry;
-            return currEntry;
+    protected Params<T> prepare(double[] pos) {
+        Throw.when()
+            .isNull(() -> pos, () -> "No function argument")
+            .isTrue(
+                () -> pos.length != this.consts.getColCount(), 
+                () -> "Invalid argument count. Expected " + this.consts.getColCount()
+                    + " but given " + pos.length
+            ); 
+        Params<T> prev = this.params;
+        if(prev != null && this.equals(pos, prev.pos)) {
+            return prev;
         }
-        return entry;
+        Matrix argsMat = this.mulT.compute(this.consts, Matrices.wrap(new double[][] {pos}));
+        double[] args = argsMat instanceof ColumnVector
+            ? ((ColumnVector) argsMat).getVector()
+            : IntStream
+                .range(0, argsMat.getRowCount())
+                .mapToDouble(i -> argsMat.get(i, 0))
+                .toArray();
+        Params<T> newParam = new Params<>(
+            Arrays.copyOf(pos, pos.length), 
+            args, 
+            this.prepare(pos, args)
+        );
+        this.params = newParam;
+        return newParam;
     }
     
     /**
-     * Compare if two double arrays are exactly equal.
-     * @param u  First array
-     * @param v  Second array
-     * @return  True if equals, false otherwise
+     * Determine if two vectors are exactly equal to each other
+     * @param u  First vector
+     * @param v  Second vector
+     * @return  True if exactly equals, false otherwise
      */
-    protected boolean deepEquals(double[] u, double[] v) {
+    protected boolean equals(double[] u, double[] v) {
+        if(u.length != v.length) {
+            return false;
+        }
         for(int i = 0; i < u.length; i++) {
             if(u[i] != v[i]) {
                 return false;
@@ -205,58 +248,73 @@ public class SumLinearArgFunc implements VectorFunction {
     }
     
     /**
-     * Compute the function arguments &lt;A, x&gt;
-     * @param pos
-     * @return
+     * Value of the underlying non-linear function in the series of functions
+     * @param inter  Intermediate computational result
+     * @param index  Function index of the series of functions
+     * @param x  Function argument
+     * @return  Value of the underlying non-linear function
      */
-    protected double[] linearArgs(double[] pos) {
-        Matrix result = this.mulT.compute(this.consts, Matrices.wrap(new double[][] {pos}));
-        if(result instanceof ColumnVector) {
-            return ((ColumnVector) result).getVector();
-        }
-        double[] column = new double[result.getRowCount()];
-        for(int i = 0; i < column.length; i++){
-            column[i] = result.get(i, 0);
-        }
-        return column;
-    }
-    
-    protected Matrix consts;    
-    protected Function<double[], FunctionSeries> funcFact;
-    protected MulT mulT;       
-    protected int flopThres;
-    protected volatile Cache prevEntry;    
+    protected abstract double valueAt(T inter, int index, double x);
     
     /**
-     * Class for cached data including function series and function arguments.
+     * Slope of the underlying non-linear function in the series of functions, i.e.
+     * the value of first derivative.
+     * @param inter  Intermediate computational result
+     * @param index  Function index of the series of functions
+     * @param x  Function argument
+     * @return  Slope of the underlying non-linear function
      */
-    protected static class Cache {
+    protected abstract double slopeAt(T inter, int index, double x);
+    
+    /**
+     * Convexity of the underlying non-linear function in the series of functions, i.e.
+     * the value of second derivative.
+     * @param inter  Intermediate computational result
+     * @param index  Function index of the series of functions
+     * @param x  Function argument
+     * @return  Convexity of the underlying non-linear function
+     */
+    protected abstract double convexityAt(T inter, int index, double x);
+    
+    /**
+     * Prepare intermediate computational result given input position and 
+     * the linear combinations with coefficients.
+     * @param pos  Input position
+     * @param args  Linear combinations with coefficients
+     * @return  Intermediary computation result
+     */
+    protected abstract T prepare(double[] pos, double[] args);
+    
+    protected final Matrix consts;
+    protected final MulT mulT;
+    protected final int flopThres;
+    
+    protected volatile Params<T> params;
+    
+    /**
+     * Data object for function arguments.
+     * 
+     * @author Y.K. Chan
+     * @param <T>  Type of intermediate computational result
+     */
+    protected static class Params<T> {
         
-        /**
-         * Input position
-         */
-        public final double[] pos;
+        private double[] pos;
         
-        /**
-         * Function arguments
-         */
-        public final double[] args;
+        private double[] args;
         
-        /**
-         * Series of functions
-         */
-        public final FunctionSeries series;
+        private T inter;
 
         /**
          * Constructor.
          * @param pos  Input position
          * @param args  Function arguments
-         * @param series  Series of functions
+         * @param inter  Intermediate computational result
          */
-        public Cache(double[] pos, double[] args, FunctionSeries series) {
+        public Params(double[] pos, double[] args, T inter) {
             this.pos = pos;
             this.args = args;
-            this.series = series;
+            this.inter = inter;
         }
         
     }
