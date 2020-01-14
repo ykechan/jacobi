bed0f834da9cf9c16beed0f0014c5a1aee862be8
eykchan1 <eykchan1@sfc.hk>
2020-01-14 17:48:40 +0800

    Unit test for AdjMatrix

--- jacobi/src/main/java/jacobi/core/graph/AdjMatrix.java
+++ jacobi/src/main/java/jacobi/core/graph/AdjMatrix.java
@@ -33,6 +33,7 @@ import java.util.stream.Stream;
 import jacobi.api.Matrix;
 import jacobi.api.graph.AdjList;
 import jacobi.api.graph.Edge;
+import jacobi.core.util.IntStack;
 import jacobi.core.util.Throw;
 
 /**
@@ -101,12 +102,15 @@ public class AdjMatrix implements AdjList {
 			return Stream.empty();
 		}
 		
-		if(map[0] < 0){
-			return this.ranges(map).mapToObj(func);
-		}
-		return Arrays.stream(map).mapToObj(func);
+		return (map[0] < 0 ? this.ranges(map) : Arrays.stream(map))
+				.mapToObj(func);
 	}
 	
+	/**
+	 * Create a integer stream from pairs of begin and end index
+	 * @param map  Pairs of begin and end index
+	 * @return  Integer stream travsing from begin to end for each pair
+	 */
 	protected IntStream ranges(int[] map) {
 		if(map.length < 4){
 			return IntStream.range(map[1], map[2]);
@@ -119,17 +123,76 @@ public class AdjMatrix implements AdjList {
 	private List<int[]> index;
 	private Matrix matrix;
 	
+	/**
+	 * Extract indices of non-zero entries from a row of adjacency matrix. 
+	 * If continuous entries dominates, this method returns the range of entries in pairs, 
+	 * with a -1 in front instead.
+	 * @param weights  Input edge weights
+	 * @param temp  Temp buffer
+	 * @return  Indices of non-zero entries, or -1 followed by range of non-zeo entries in pairs, or
+	 *          null if all entries are non-zero
+	 */
 	protected static int[] scatters(double[] weights, int[] temp) {
-		int k = 0;
+		int k = 0;	
+		int br = 0;
 		for(int i = 0; i < weights.length; i++){
-			if(weights[i] != 0.0){
-				temp[k++] = i; 
+			if(weights[i] != 0.0) {				
+				temp[k++] = i;
+				continue;
+			}			
+			
+			if(k > 0 && temp[k - 1] == i - 1){
+				br++;
 			}
-		}
-		return Arrays.copyOfRange(temp, 0, k);
-	}		
+		}				
 		
-	protected static final double DEFAULT_SPARSE_RATIO = 0.6;
+		if(k == weights.length) {
+			// fully connected
+			return null;
+		}
+		
+		return RATIO_TO_EDGES * br < k && RATIO_TO_VERTICES * br < weights.length
+			? runLength(temp, k) 
+			: Arrays.copyOfRange(temp, 0, k);
+	}
 	
-	protected static final double DEFAULT_DENSE_RATIO = 0.8;
+	/**
+	 * Encode separate indices to pairs of begin and end indices, with -1 at front.
+	 * @param array  Input array of indices
+	 * @param length  Length of input array of interest
+	 * @return  -1 followed by range of non-zeo entries in pairs
+	 */
+	protected static int[] runLength(int[] array, int length) {
+		IntStack stack = new IntStack(4);
+		stack.push(-1);
+		
+		int offset = array[0];
+		int span = 1;
+		
+		for(int i = 1; i < length; i++) {
+			if(array[i] == offset + span){
+				span++;
+				continue;
+			}
+			
+			stack.push(offset).push(offset + span);
+			offset = array[i];
+			span = 1;
+		}
+		
+		if(span > 0) {
+			stack.push(offset).push(offset + span);
+		}
+		return stack.toArray();
+	}
+	
+	/**
+	 * Minimum ratio of total number of edges to number of ranges to use pair of ranges
+	 */
+	protected static final long RATIO_TO_EDGES = 3L;
+	
+	/**
+	 * Minimum ratio of total number of vertices to number of ranges to use pair of ranges
+	 */
+	protected static final long RATIO_TO_VERTICES = 10L;
 }
--- jacobi/src/test/java/jacobi/core/graph/AdjMatrixTest.java
+++ jacobi/src/test/java/jacobi/core/graph/AdjMatrixTest.java
@@ -1,6 +1,7 @@
 package jacobi.core.graph;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -56,18 +57,88 @@ public class AdjMatrixTest {
 	}	
 	
 	@Test
-	public void shouldBeAbleToEncodeNonZeroList() {
-		Assert.assertArrayEquals(new int[] {0, 1, 2, 3, 4, 6, 7, 8}, AdjMatrix.scatters(new double[] {
-			1.0, 2.0, 3.0, 4.0, 5.0, 0.0, 6.0, 7.0, 8.0
-		}, new int[100]));
-		
-		Assert.assertArrayEquals(new int[] {0, 1, 2, 3, 7, 8}, AdjMatrix.scatters(new double[] {
-			1.0, 2.0, 3.0, 4.0, 0.0, 0.0, 0.0, 7.0, 8.0
-		}, new int[100]));
+	public void shouldBeAbleToEncodeSparseList() {
 		
 		Assert.assertArrayEquals(new int[] {0, 2, 4}, AdjMatrix.scatters(new double[] {
 			1.0, 0.0, 2.0, 0.0, 3.0
 		}, new int[100]));
+		
+		Assert.assertArrayEquals(new int[] {2, 4, 9}, AdjMatrix.scatters(new double[] {
+			0.0, 0.0, 1.0, 0.0, 3.0, 0.0, 0.0, 0.0, 0.0, 9.0, 0.0
+		}, new int[100]));
+	}
+	
+	@Test
+	public void shouldBeAbleToReturnNullIfAllAreNonZero() {
+		double[] weights = new double[100];
+		Arrays.fill(weights, Math.PI);
+		
+		Assert.assertNull(AdjMatrix.scatters(weights, new int[weights.length]));
+	}
+	
+	@Test
+	public void shouldBeAbleToReturnSparseIndexIfEntriesAreFew() {		
+		int[] map = AdjMatrix.scatters(new double[]{
+			0.0, 0.0, 0.0, 
+			1.0, 0.0, 3.0, 
+			0.0, 0.0, 0.0, 
+			0.0}, new int[100]);
+		Assert.assertArrayEquals(new int[] {3, 5}, map);
+	}
+	
+	@Test
+	public void shouldBeAbleToReturnSparseIndexIfEntriesAreNotContinuous() {	
+		int[] map = AdjMatrix.scatters(IntStream.range(0, 30)
+				.mapToDouble(i -> i % 2 == 0 ? Math.PI : 0)
+				.toArray(),  
+				new int[100]);
+		
+		Assert.assertArrayEquals(
+			IntStream.range(0, 30).filter(i -> i % 2 == 0).toArray(), 
+			map
+		);
+	}
+	
+	@Test
+	public void shouldBeAbleToReturnSparseIndexIfContinuousRangeBreaksTooOften() {	
+		int[] map = AdjMatrix.scatters(IntStream.range(0, 30)
+				.mapToDouble(i -> i % 4 == 0 ? 0 : Math.PI)
+				.toArray(),  
+				new int[100]);
+		
+		Assert.assertArrayEquals(
+			IntStream.range(0, 30).filter(i -> i % 4 > 0).toArray(), 
+			map
+		);
+	}
+	
+	@Test
+	public void shouldBeAbleToReturnRangePairsWhenOnly1ContinousRange() {
+		double[] weights = new double[30];
+		Arrays.fill(weights, 3, 23, Math.E);
+		
+		Assert.assertArrayEquals(new int[] {-1, 3, 23}, AdjMatrix.scatters(weights, new int[100]));
+	}
+	
+	@Test
+	public void shouldBeAbleToReturnRangePairsWhenAreMultipleRanges() {
+		double[] weights = IntStream.range(0, 100)
+				.mapToDouble(i -> i % 10 == 0 ? 0 : Math.PI)
+				.toArray();
+		
+		//Assert.assertArrayEquals(new int[] {-1, 3, 23}, AdjMatrix.scatters(weights, new int[100]));
+		int[] map = AdjMatrix.scatters(weights, new int[100]);
+		Assert.assertEquals(-1, map[0]);
+		for(int i = 1; i < map.length; i += 2) {
+			int off = 10 * ((i - 1) / 2);
+			Assert.assertEquals(off + 1, map[i]);
+			Assert.assertEquals(off + 10, map[i + 1]);
+		}
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldFailWhenInputMatrixIsNotSquare() {
+		AdjMatrix.of(Matrices.zeros(3, 2));
 	}
 	
 
