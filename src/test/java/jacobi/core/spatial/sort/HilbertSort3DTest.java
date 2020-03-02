f7d780541d20a4da7d143d931f480fd5865377bc
eykchan1 <eykchan1@sfc.hk>
2020-03-02 14:40:28 +0800

    Test for Hilbert Sort 3D

--- src/test/java/jacobi/core/spatial/sort/HilbertSort3DTest.java
+++ src/test/java/jacobi/core/spatial/sort/HilbertSort3DTest.java
@@ -101,7 +101,7 @@ public class HilbertSort3DTest {
 	}
 	
 	@Test
-	public void shouldBeAbleToSortA4x4x4CubeWithFirstBasis() {
+	public void shouldBeAbleToSortA4x4x4CubeByFirstBasis() {
 		List<double[]> points = this.grid(4);
 		HilbertSort3D sorter = new HilbertSort3D(0, 1, 2);
 		
@@ -110,6 +110,46 @@ public class HilbertSort3DTest {
 			HilbertSort3D.BASIS[0], 
 			IntStream.range(0, points.size()).toArray()
 		);			
+		
+		for(int k = 1; k < order.length; k++) {
+			double[] u = points.get(order[k - 1]);
+			double[] v = points.get(order[k]);
+			
+			Assert.assertEquals(1.0, 
+				Math.abs(u[0] - v[0])
+			  + Math.abs(u[1] - v[1])
+			  + Math.abs(u[2] - v[2]), 1e-8);
+		}
+	}
+	
+	@Test
+	public void shouldBeAbleToSortA4x4x4CubeByAnyBasis() {
+		List<double[]> points = this.grid(4);
+		HilbertSort3D sorter = new HilbertSort3D(0, 1, 2);
+		
+		for(int basis : HilbertSort3D.BASIS) {
+			double[] comps = sorter.init(points);
+			int[] order = sorter.sort(comps, 
+				basis, 
+				IntStream.range(0, points.size()).toArray()
+			);			
+			
+			for(int k = 1; k < order.length; k++) {
+				double[] u = points.get(order[k - 1]);
+				double[] v = points.get(order[k]);
+				
+				Assert.assertEquals(1.0, 
+					Math.abs(u[0] - v[0])
+				  + Math.abs(u[1] - v[1])
+				  + Math.abs(u[2] - v[2]), 1e-8);
+			}
+		}
+	}
+	
+	@Test
+	public void shouldBeAbleToSortA16x16x16Cube() {
+		List<double[]> points = this.grid(16);
+		int[] order = new HilbertSort3D(0, 1, 2).sort(points);
 		
 		for(int k = 1; k < order.length; k++) {
 			double[] u = points.get(order[k - 1]);
